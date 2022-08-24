e022331082470b6b114d31892ca7f35825ac2a7e
sockmonkeyy <mr.richardson718@gmail.com>
2022-08-24 18:11:27 -0500

    base complete

--- app.js
+++ app.js
@@ -5,3 +5,38 @@ text.innerHTML = text.innerText
         (char, i) => `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`
     )
     .join("");
+
+      /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  /* customize formbutton below*/     
+ let formBtn = formbutton("create", {
+    
+    action: "https://formspree.io/f/xdojkzaq",
+    title: "How can we help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "rgb(232, 159, 48)"
+      },
+      button: {
+        backgroundColor: "rgb(232, 159, 48)"
+      }
+    }
+    
+  });
+ 
