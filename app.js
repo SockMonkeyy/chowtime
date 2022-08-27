c92f85290422980e01e34f6b5fc4843e82a42a66
sockmonkeyy <mr.richardson718@gmail.com>
2022-08-27 13:39:29 -0500

    added fadeIn animations

--- app.js
+++ app.js
@@ -39,4 +39,67 @@ text.innerHTML = text.innerText
     }
     
   });
+
+  /*time function*/
+var now = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  let checkTime = function () {
+      let today = weekday[now.getDay()];
+      let timeDiv = document.getElementById('timeDiv');
+      let dayOfWeek = now.getDay();
+      let hour = now.getHours();
+      let minutes = now.getMinutes();
+      let time = now.getTime();
+
+
+      //add AM or PM
+      var suffix = hour >= 12 ? "P.M." : "A.M.";
+
+      // add 0 to one digit minutes
+      if (minutes < 10) {
+          minutes = "0" + minutes
+      };
+
+      if ((dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 6) && hour >= 10 && hour <= 13) {
+          hour = ((hour + 11) % 12 + 1); //i.e. show 1:15 instead of 13:15
+
+          timeDiv1.innerHTML = `It's ${today} ${hour}:${minutes} ${suffix} we're open and accepting your orders`;
+          timeDiv1.className = 'open2';
+      }
+
+      else if ((dayOfWeek == 3 || dayOfWeek == 4 || dayOfWeek == 5) && hour >= 10 && hour <= 13) {
+          hour = ((hour + 11) % 12 + 1);
+
+          timeDiv1.innerHTML = `It's ${today} ${hour}:${minutes} ${suffix} we're open accepting and your orders`;
+          timeDiv1.className = 'open2';
+      }
+
+      else {
+          if (dayofweek = 0 || hour == 0 || hour > 12) {
+              hour = ((hour + 11) % 12 + 1); //i.e. show 1:15 instead of 13:15
+          }
+
+          timeDiv1.innerHTML = `It's ${today} ${hour}:${minutes} ${suffix} sorry we're closed, but it's never to early to pre-order your next meal \n\n   We Open at 10:00 A.M.`;
+          timeDiv1.className = 'closed2'
+
+      }
+
+  };
+  var currentDay = weekday[now.getDay()];
+  var currentDayID = "#" + currentDay; //gets todays weekday and turns it into id
+  $(currentDayID).toggleClass("today"); //hightlights today in the view hours modal popup
+
+  setInterval(checkTime, 1000);
+  checkTime();
+
+
+
  
